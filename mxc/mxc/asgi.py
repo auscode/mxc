381b8b946217026e80bd4bc3d99a3854ac493e8a
auscode <hm6091999@gmail.com>
2023-11-14 20:25:33 +0530

    first commit

---
+++ mxc/mxc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mxc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mxc.settings')
+
+application = get_asgi_application()
